20608c21fb96a084dc014c2f75b24a59aada7e8f
Ludovic <ludovic.porlan@gmail.com>
2021-11-23 16:32:54 +0100

    add mongodb

--- back/app.js
+++ back/app.js
@@ -1,9 +1,33 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 
-app.use((req, res) => {
+// Connexion à la base de donnée
+mongoose
+    .connect(
+        "mongodb+srv://Otucko:1DBCKXBg65nDwdOcuvk2@hottakespiquante.yuuks.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log("Connexion à MongoDB réussie !"))
+    .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use((req, res, next) => {
+    console.log("Requête reçue !");
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
     res.json({ message: "Votre requête a bien été reçue !" });
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("Réponse envoyée avec succès !");
 });
 
 module.exports = app;
