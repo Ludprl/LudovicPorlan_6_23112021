491728b994a44063802956d173285c06dc2558a5
Ludovic <ludovic.porlan@gmail.com>
2021-12-11 15:20:52 +0100

    add helmet

--- back/app.js
+++ back/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
+const helmet = require("helmet");
 
 const userRoutes = require("./routes/user");
 const sauceRoutes = require("./routes/sauce");
@@ -35,6 +36,8 @@ app.use((req, res, next) => {
 /**
  * Middlewares > bodyParser /
  */
+// Usage de helmet pour protéger le header
+app.use(helmet());
 
 // Parse du body des requetes en json
 app.use(bodyParser.json());
